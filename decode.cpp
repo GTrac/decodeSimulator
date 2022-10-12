abfae5fb77447d0bf2e5efb459b27abbafdbe92d
gtraczyk <traczyk.gilbert@gmail.com>
2022-10-12 13:51:30 -0400

    control signals

--- decode.cpp
+++ decode.cpp
@@ -14,10 +14,10 @@ void decode(uint32_t instruction){
         bitset<5> rs1 = (uint8_t)((instruction >> 15) & 0b11111);
         bitset<3> funct3 = (uint8_t)((instruction >> 12) & 0b111);
         bitset<5> rd = (uint8_t)((instruction >> 7) & 0b11111);
-
-        cout << "Control Signals:\nreg_wen: " << x << " reg_ren: " << x << " alu_op: " << funct7[5] << funct3.to_string() << "\nmem_wen: " << x << " mem_ren: " << x;
-        cout << " sel1: " << opCode[0] << " sel2: " << opCode[1] << "\nsel3: " << opCode[2] << " sel4: " << opCode[3] << " sel5: " << opCode[4] << " sel6: " << opCode[5] << " sel7: " << opCode[6];
-        cout << " branch: " << x << " auipc: " << x << " lui: " << x << endl;
+        bool sel8 = funct7[0] && (funct3[2]==0) && (funct3[1] || funct3[0]);
+        cout << "Control Signals:\nreg_wen: " << 1 << " reg_ren: " << 1 << " alu_op: " << funct7[5] << funct3.to_string() << "\nmem_wen: " << 0 << " mem_ren: " << 0;
+        cout << " sel1: " << 0 << " sel2: " << 0 << "\nsel3: " << 0 << " sel4: " << 0 << " sel5: " << 0 << " sel6: " << 1 << " sel7: " << 0 << " sel8: " << sel8;
+        cout << " branch: " << 0 << " auipc: " << 0 << " lui: " << 0 << endl;
     }else if(opCode == 0b0010011 | opCode == 0b0000011 | opCode == 0b0011011){
         cout << "Opcode Type: I" << endl;
         bitset<12> imm{instruction >> 20};
@@ -25,6 +25,15 @@ void decode(uint32_t instruction){
         bitset<5> rs1 = (uint8_t)((instruction >> 15) & 0b11111);
         bitset<3> funct3 = (uint8_t)((instruction >> 12) & 0b111);
         bitset<5> rd = (uint8_t)((instruction >> 7) & 0b11111);
+        if(opCode == 0b0010011){
+            cout << "Control Signals:\nreg_wen: " << 1 << " reg_ren: " << 1 << " alu_op: " << imm[10] << funct3.to_string() << "\nmem_wen: " << 0 << " mem_ren: " << 0;
+            cout << " sel1: " << 1 << " sel2: " << 1 << "\nsel3: " << 0 << " sel4: " << 0 << " sel5: " << 0 << " sel6: " << 1 << " sel7: " << 0 << " sel8: " << 0;
+            cout << " branch: " << 0 << " auipc: " << 0 << " lui: " << 0 << endl;
+        }else if(opCode == 0b0000011){
+            cout << "Control Signals:\nreg_wen: " << 1 << " reg_ren: " << 1 << " alu_op: " << 0 << funct3.to_string() << "\nmem_wen: " << 0 << " mem_ren: " << 0;
+            cout << " sel1: " << 0 << " sel2: " << 1 << "\nsel3: " << 1 << " sel4: " << 0 << " sel5: " << 0 << " sel6: " << 1 << " sel7: " << 0 << " sel8: " << 0;
+            cout << " branch: " << 0 << " auipc: " << 0 << " lui: " << 0 << endl;
+        }
         
 
     }else if(opCode == 0b0100011){
@@ -42,15 +51,17 @@ void decode(uint32_t instruction){
         bitset<5> rs1 = (uint8_t)((instruction >> 15) & 0b11111);
         bitset<3> funct3 = (uint8_t)((instruction >> 12) & 0b111);
         cout << "imm: " << imm << endl;
-           
+        
     }else if(opCode == 0b0110111 || opCode == 0b0010111){
         cout << "Opcode Type: U" << endl;
         bitset<20> imm{((instruction>>12 & 0b111111)<<5) + ((instruction>>31)<<20) + ((instruction>>7 & 0b01)<<11) + (instruction>>30)};
         bitset<5> rd = (uint8_t)((instruction >> 7) & 0b11111);
+
     }else if(opCode == 0b1101111){
         cout << "Opcode Type: UJ" << endl;
         bitset<20> imm{(instruction>>12)};
         bitset<5> rd = (uint8_t)((instruction >> 7) & 0b11111);
+
     }else{
         cout << "\n/////////////////ERROR//////////////" << endl;
     }
